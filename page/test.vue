ee9dc2b6b68464ed2d68c0c998166416e82719cc
SongJingxin <1944981402@qq.com>
2021-07-08 11:25:06 +0800

    Create test.vue
    创建并修改

---
+++ page/test.vue
@@ -0,0 +1,20 @@
+<!--  -->
+<template>
+  <div class="">{{ title }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "测试",
+    };
+  },
+  components: {},
+  computed: {},
+  watch: {},
+  methods: {},
+};
+</script>
+<style lang='' scoped>
+</style>
